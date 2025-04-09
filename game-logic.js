aad4c6256c3a0799e8efecbfef3fd8a396847168
codersimone <97359905+codersimone@users.noreply.github.com>
2025-04-09 23:47:55 +0300

    Almost complete step 5 of the task, created a function with the logic of conducting one round and the rules for winning the round. It is necessary to figure out why it does not increase the score after the round.

--- game-logic.js
+++ game-logic.js
@@ -1,29 +1,69 @@
-// Through an object and its methods
-// Computer choice
+// through an object and its methods
+const gameRules = {
+  "paper": "rock",
+  "scissors": "paper",
+  "rock": "scissors"
+};
+
+// computer choice
 function getComputerChoice() {
-  const options = {
-    0: "rock",
-    1: "paper",
-    2: "scissors"
-  };
   // getting the keys of the gameOptions object
-  const optionsKeys = Object.keys(options);
+  const gameRulesKeys = Object.keys(gameRules);
   // generating a random index of the gameOptions object (computer choice)
-  const optionsRandomIndex = Math.floor(Math.random() * optionsKeys.length);
-  // Returning the value corresponding to a random key and assigns it to a new variable
-  const computerChoiceValue = options[optionsKeys[optionsRandomIndex]]
-  return console.log("Computer choice: " + computerChoiceValue);
-}
+  const randomIndex = Math.floor(Math.random() * gameRulesKeys.length);
+  // returning the value corresponding to a random key and assigns it to a variable
+  const choiceResult = gameRules[gameRulesKeys[randomIndex]];
+  // output the computer selection to the console separately, for clarity, so as not to change the value received by the choice variable and passed to the playRound function
+  console.log("Computer have entered: " + choiceResult);
+  return choiceResult;
+};
 
-getComputerChoice();
-
-// Human choice
+// human choice
 function getHumanChoice() {
-  const humanChoiceValue = prompt("Let's play! Choose your option and enter it in the field below: rock, paper or scissors", ""); 
-  return console.log("Human choice: " + humanChoiceValue);
-}
+  // request and receive the value of the person's choice, reduce the resulting string to lowercase for insensitivity of the value entered by the person and assign it to a variable
 
-getHumanChoice();
 
+
+  const inputResult = prompt("Let's play! Choose your option and enter it in the field below: rock, paper or scissors", "").toLowerCase();
+  // output the person's choice to the console separately, for clarity, so as not to change the value received by the input variable and passed to the playRound function.
+  console.log("You have entered: " + inputResult);
+  return inputResult;
+};
+
+// global variables for storing the count of rounds with starting values of 0.
 const humanScore = 0;
 const computerScore = 0;
+
+// single round function with humanChoice and computerChoice parameters 
+function playRound(humanChoice, computerChoice) {
+  // rules for determining the winner of the round
+  if (humanChoice === computerChoice) {
+    console.log("A draw!");
+  } else if (gameRules[humanChoice] === computerChoice) {
+    console.log("You've won this round!");
+    // increasing a person's score after a round
+
+    // DOES NOT INCREASE THE SCORE OF THE ROUND AND GIVES AN ERROR - FIND OUT WHY !!!
+    // problem with the values being received due to the text for the console, as was the case with the first 3 functions???
+    humanScore++;
+
+  } else {
+    console.log("The computer won this round!");
+    // increasing the computer's score after a round
+
+    // DOES NOT INCREASE THE SCORE OF THE ROUND AND GIVES AN ERROR - FIND OUT WHY !!!
+    // problem with the values being received due to the text for the console, as was the case with the first 3 functions???
+    computerScore++;
+
+  }
+
+  // output of the current account of the round to the console 
+  console.log(`Current score: You: ${humanScore}, Computer: ${computerScore}`);
+};
+
+// call the human and computer selection functions and assign them to variables
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+// using the humanChoice and computerChoice parameters to accept human and computer choices as arguments when calling a function
+playRound(humanSelection, computerSelection);
