58b2fc5cc4b8270c797bad868d7b535add8febc0
codersimone <97359905+codersimone@users.noreply.github.com>
2025-04-09 09:38:53 +0300

    Changed the solution from an array to an object

--- game-logic.js
+++ game-logic.js
@@ -1,9 +1,29 @@
-const gameОptions = ["rock", "paper", "scissors"];
+// Through an array and its methods
+// const gameОptions = ["rock", "paper", "scissors"];
+
+// Computer choice
+// function getComputerChoice() {
+//   return gameОptions[Math.floor(Math.random() * gameОptions.length)];
+// }
+// console.log(getComputerChoice());
+
+// Through an object and its methods
+const gameОptions = {
+  0: "rock",
+  1: "paper",
+  2: "scissors"
+};
 
 // Computer choice
 function getComputerChoice() {
-  return gameОptions[Math.floor(Math.random() * gameОptions.length)];
+  // getting the keys of the gameOptions object
+  const gameOptionsKeys = Object.keys(gameОptions);
+  // generating a random index of the gameOptions object (computer choice)
+  const computerChoiceRandomIndex = Math.floor(Math.random() * gameOptionsKeys.length);
+  // Returning the value corresponding to a random key
+  return gameОptions[gameOptionsKeys[computerChoiceRandomIndex]];
 }
+
 console.log(getComputerChoice());
 
 // Human choice
