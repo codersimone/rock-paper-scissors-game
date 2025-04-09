8caf10a20b2e1f9adc4b4e00aa951dc6ced2af0f
codersimone <97359905+codersimone@users.noreply.github.com>
2025-04-09 14:40:19 +0300

    Deleted the style file and the initial html markup for the game via the console, changed the solution from arrays to objects and wrote the first two functions getComputerChoice and getHumanChoice.

--- game-logic.js
+++ game-logic.js
@@ -1,32 +1,27 @@
-// Through an array and its methods
-// const gameОptions = ["rock", "paper", "scissors"];
-
-// Computer choice
-// function getComputerChoice() {
-//   return gameОptions[Math.floor(Math.random() * gameОptions.length)];
-// }
-// console.log(getComputerChoice());
-
 // Through an object and its methods
-const gameОptions = {
-  0: "rock",
-  1: "paper",
-  2: "scissors"
-};
-
 // Computer choice
 function getComputerChoice() {
+  const options = {
+    0: "rock",
+    1: "paper",
+    2: "scissors"
+  };
   // getting the keys of the gameOptions object
-  const gameOptionsKeys = Object.keys(gameОptions);
+  const optionsKeys = Object.keys(options);
   // generating a random index of the gameOptions object (computer choice)
-  const computerChoiceRandomIndex = Math.floor(Math.random() * gameOptionsKeys.length);
-  // Returning the value corresponding to a random key
-  return gameОptions[gameOptionsKeys[computerChoiceRandomIndex]];
+  const optionsRandomIndex = Math.floor(Math.random() * optionsKeys.length);
+  // Returning the value corresponding to a random key and assigns it to a new variable
+  const computerChoiceValue = options[optionsKeys[optionsRandomIndex]]
+  return console.log("Computer choice: " + computerChoiceValue);
 }
 
-console.log(getComputerChoice());
+getComputerChoice();
 
 // Human choice
 function getHumanChoice() {
-
+  const humanChoiceValue = prompt("Let's play! Choose your option and enter it in the field below: rock, paper or scissors", ""); 
+  return console.log("Human choice: " + humanChoiceValue);
 }
+
+getHumanChoice();
+
