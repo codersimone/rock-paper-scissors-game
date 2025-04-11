aa4837ad15263a01b7f7329c70730f500690a10b
codersimone <97359905+codersimone@users.noreply.github.com>
2025-04-11 15:41:24 +0300

    Deleted unnecessary comments.

--- game-logic.js
+++ game-logic.js
@@ -5,12 +5,10 @@ const gameRules = {
 };
 
 function getHumanChoice() {
-    // request and receive the value of the person's choice, reduce the resulting string to lowercase for insensitivity of the value entered by the person and assign it to a variable
     const inputResult = prompt(
         "Let's play! Choose your option and enter it in the field below: rock, paper or scissors",
         ''
     ).toLowerCase();
-    // output the person's choice to the console separately, for clarity, so as not to change the value received by the input variable and passed to the playRound function.
     console.log('You have entered: ' + inputResult);
     return inputResult;
 }
@@ -18,25 +16,19 @@ function getHumanChoice() {
 function getComputerChoice() {
     // getting the keys of the gameOptions object
     const gameRulesKeys = Object.keys(gameRules);
-    // generating a random index of the gameOptions object (computer choice)
+    // generating a random index of the gameOptions object
     const randomIndex = Math.floor(Math.random() * gameRulesKeys.length);
-    // returning the value corresponding to a random key and assigns it to a variable
     const choiceResult = gameRules[gameRulesKeys[randomIndex]];
-    // output the computer selection to the console separately, for clarity, so as not to change the value received by the choice variable and passed to the playRound function
     console.log('Computer have entered: ' + choiceResult);
     return choiceResult;
 }
 
 function playGame() {
-    // global variables for storing the count of rounds with starting values of 0.
     let humanScore = 0;
     let computerScore = 0;
 
-    // single round function with humanChoice and computerChoice parameters
     function playRound(humanChoice, computerChoice) {
-        // rules for determining the winner of the round
         if (gameRules[humanChoice] === computerChoice) {
-            // increasing a person's score after a round
             humanScore++;
             console.log(
                 `You've won this round! Current score: You: ${humanScore}, Computer: ${computerScore}`
@@ -45,7 +37,6 @@ function playGame() {
         }
 
         if (gameRules[computerChoice] === humanChoice) {
-            // increasing the computer's score after a round
             computerScore++;
             console.log(
                 `The computer won this round! Current score: You: ${humanScore}, Computer: ${computerScore}`
@@ -58,17 +49,13 @@ function playGame() {
         );
     }
 
-    // a for loop for calling the playground function 5 times in a row
     for (let i = 0; i < 5; i++) {
-        // call the human and computer selection functions and assign them to variables
         const humanSelection = getHumanChoice();
         const computerSelection = getComputerChoice();
 
-        // using the humanChoice and computerChoice parameters to accept human and computer choices as arguments when calling a function
         playRound(humanSelection, computerSelection);
     }
 
-    // determine the winner based on 5 rounds and output one of the received values to the console
     if (humanScore > computerScore) {
         console.log("Congratulations, you've won the game!");
         return;
